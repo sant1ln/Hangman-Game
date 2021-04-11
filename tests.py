312970990671971a874698f58fbb1678684cd22d
Santiago <santiagoln09@gmail.com>
2021-04-10 23:12:20 -0500

    shit, finaly <3

--- tests.py
+++ tests.py
@@ -21,3 +21,27 @@ print('_ '*longitud)
 if(is_letter):
     pass
 
+""" ______________________________________________________________________________ """
+
+random_word = 'ana'
+palabra = []
+for letra in random_word:
+    palabra.append(letra)
+print(palabra)
+
+
+def lienarSearch(palabra,listLen,letter):
+    acertions = []
+    for i in range(0,listLen):
+        if(palabra[i] == letter):
+            acertions.append(i)
+    return acertions
+
+letter = 'a'
+listLen = len(palabra)
+result = lienarSearch(palabra,listLen,letter)
+
+if(result):
+    print("Element found at index: ",result)
+else:
+    print("Not found")
